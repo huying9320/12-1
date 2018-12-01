0858d49c15c384fb1c38a8828b6da8903b83dd49
huying <1660224836@qq.com>
2018-12-01 12:18:26 +0800

    内容-swiper-渲染数据

--- gulp/js/index.js
+++ gulp/js/index.js
@@ -1,11 +1,40 @@
+/*
+ * @Author: huying 
+ * @Date: 2018-12-01 11:46:39 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-12-01 12:17:35
+ */
+var wrap = document.querySelector('.main-wrap');
+
 var xml = new XMLHttpRequest();
 xml.open('get', '/api/data', true);
 xml.onload = function(res) {
     if (res.target.status === 200) {
         var data = JSON.parse(res.target.response)
-        console.log(data)
+        xrswiper(data.result)
     }
 }
 xml.send()
 
-new Swiper('.main-ban')
+/**
+ * [xrswiper description]
+ *
+ * @param   {[type]}  data  [渲染数据的data]
+ *
+ * @return  {[type]}        [return description]
+ */
+function xrswiper(data) {
+    var html = '';
+    data.forEach(function(file) {
+        html += `<div class="swiper-slide">`
+        file.forEach(function(item) {
+            html += `<dl>
+                        <dt><img src="${item.url}" alt=""></dt>
+                        <dd>${item.name}</dd>
+                    </dl>`;
+        })
+        html += `</div>`;
+    })
+    wrap.innerHTML = html;
+    new Swiper('.main-ban');
+}
