839e88084780b3208c697f8441232208cc291810
huying <1660224836@qq.com>
2018-12-02 20:02:39 +0800

    项目完成

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -18,7 +18,8 @@ var path = require('path');
 var fs = require('fs');
 var data = require('./mock/data.json');
 
-
+// es6--->es5 语法
+// var babel = require('gulp-babel');
 
 // 生成css文件
 gulp.task('default', function() {
@@ -46,7 +47,7 @@ gulp.task('devWatch', function() {
 
 // 启动服务
 gulp.task('server', function() {
-    return gulp.src('../gulp')
+    return gulp.src('../build')
         .pipe(server({
             port: 8060, //配置端口
             open: true, //自动打开浏览器
@@ -55,17 +56,16 @@ gulp.task('server', function() {
             // fallback: 'index.html', //指定默认文件
             middleware: function(req, res, next) { // 拦截前端请求
                 var pathname = url.parse(req.url).pathname;
-                if (req.url === '/favicon.ico') {
+                if (req.url === '/favicon.ico' || req.url === '/js/lib/swiper.min.js.map') {
                     return res.end('');
                 }
                 if (pathname === '/api/data') {
-                    console.log(123)
                     res.end(JSON.stringify({ code: 0, msg: '加载成功', result: data }))
                 } else {
                     // 加载首页
                     pathname = pathname === '/' ? 'index.html' : pathname;
                     console.log(pathname);
-                    res.end(fs.readFileSync(path.join(__dirname, '../gulp', pathname)));
+                    res.end(fs.readFileSync(path.join(__dirname, '../build', pathname)));
                 }
             }
         }))
@@ -76,10 +76,45 @@ gulp.task('dev', gulp.parallel('Scss', 'server', 'devWatch'));
 
 
 
-// 线上环境
+// 整到线上环境build
+
+gulp.task('bcss', function() {
+    return gulp.src('./css/*.css')
+        .pipe(gulp.dest('../build/css'))
+})
+
 // 压缩js
 gulp.task('buglify', function() {
-    return gulp.src(['./js/**/*.js', '!./js/libs/*.js'])
-        .pipe(uglify())
+    return gulp.src(['./js/*.js', '!./js/lib/*.js'])
+        // .pipe(babel({
+        //     presets: ['@babel/env']
+        // }))
+        // .pipe(uglify())
         .pipe(gulp.dest('../build/js'))
 })
+
+// 项目引入的拷贝js
+gulp.task('bcopyjs', function() {
+    return gulp.src('./js/lib/*.js')
+        .pipe(gulp.dest('../build/js/lib'))
+})
+
+// 压缩html
+gulp.task('bhtml', function() {
+    return gulp.src('./*.html')
+        .pipe(gulp.dest('../build'))
+})
+
+// Image
+gulp.task('bimg', function() {
+    return gulp.src('./img/*.jpg')
+        .pipe(gulp.dest('../build/img'))
+})
+
+gulp.task('bfonts', function() {
+    return gulp.src('./fonts/*.*')
+        .pipe(gulp.dest('../build/fonts'))
+})
+
+// 线上环境
+gulp.task('bdev', gulp.series('bcss', 'bimg', 'buglify', 'bfonts', 'bcopyjs', 'bhtml'));
